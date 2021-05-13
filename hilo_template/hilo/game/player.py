0b91cd70ea62494dc1c3f5cb35f7d6f84c7e1b2d
Joseph R. Stewart <JosephRobertStewart@gmail.com>
2021-05-13 15:02:50 -0600

    Added class doc comments to player.py

--- hilo_template/hilo/game/player.py
+++ hilo_template/hilo/game/player.py
@@ -2,18 +2,44 @@
                 # translates
 
 class Player:
+    """[This is a class documentation block comment.]
+    (What this is)
+    (What the methods are and do)
+    
+    Attributes: [attributes are just variables with data]
+        option:
+        keep_playing:
+        card_guess:
+        
+    """
     def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
+
+        Args:
+            self (Player): An instance of Player.
+        """
         self.option = "y"
         self.keep_playing = True
         self.card_guess = ""
 
     def choice(self):
+        """Decides whether to keep playing the game or not. 
+
+        Args: 
+            self (Player): An instance of Player.
+        """
         self.option = input("Keep Playing y/n ")
         if self.option == "n":
             self.keep_playing = False
 
     def guess(self):
+        """Decides whether the guess is higher or lower. 
+
+        Args: 
+            self (Player): An instance of Player.
+        """
         self.card_guess = input("Higher or Lower? h/l ")
+        
         
     #Do we need this, below?
 """
