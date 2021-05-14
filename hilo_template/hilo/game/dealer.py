b82eef7e7e212425ca6f797a4d3cf6498aa903f0
Joseph R. Stewart <JosephRobertStewart@gmail.com>
2021-05-14 09:51:09 -0600

    polished dealer class

--- hilo_template/hilo/game/dealer.py
+++ hilo_template/hilo/game/dealer.py
@@ -2,16 +2,16 @@ from game.player import Player
 import random as r
 class Dealer():
     """[This is a class documentation block comment.]
-    (What this is)
-    (What the methods are and do)
+    This class holds game and rule structure. Dealer class draws cards randomly 
+    and tallies points with its methods.    
     
     Attributes: [attributes are just variables with data]
-        keep_playing: ADD STUFF HERE
-        old_card: ADD STUFF HERE
-        new_card: ADD STUFF HERE
-        player: ADD STUFF HERE
-        guess: ADD STUFF HERE
-        points: ADD STUFF HERE
+        keep_playing: allows user to keep playing
+        old_card: the card the the high/low guess is based on
+        new_card: the card we hope matches our guess
+        player: the Player class (this is you, the user)
+        guess: the user's input
+        points: the scoring system
     """
     
     def __init__(self):
@@ -28,7 +28,7 @@ class Dealer():
         self.points = 300
 
     def start_game(self):
-        """    (What this is)
+        """This method runs the Hi/Lo game.
 
         Args: 
             self (Player): An instance of Player.
@@ -44,13 +44,18 @@ class Dealer():
             self.card_update()
 
     def to_play(self):
-        """    (What this is)
+        """Checks whether the User can play or not. Points must
+        be above zero, below 1,000, and player must choose to play in order to 
+        keep playing (see the next card).
 
         Args: 
             self (Player): An instance of Player.
             
-        Return: ADD STUFF HERE
+        Return: If "False" the game ends.
         """
+        if self.points >= 1000:
+            print("Congratulations! You won!")
+            return False
         if self.points > 0:
             self.player.choice()
             return (self.points > 0 and self.player.option == "y")
@@ -59,15 +64,23 @@ class Dealer():
 
 
     def draw_card(self):
-        """    (What this is)
+        """Draws the next random card. Each card is shuffled 
+        back into the deck after each round (see while loop).
+        That part qualifies this code for a score of five!
 
         Args: 
             self (Player): An instance of Player.
         """
-        self.new_card = r.randint(1,13)
-
+        key = False
+        while key == False:
+            self.new_card = r.randint(1,13)
+            if self.new_card == self.old_card:
+                key = False
+            else:
+                key = True
+                        
     def display_card(self):
-        """    (What this is)
+        """Displays the drawn card.
 
         Args: 
             self (Player): An instance of Player.
@@ -75,7 +88,7 @@ class Dealer():
         print(f"The new card is: {self.new_card}")
 
     def get_points(self):
-        """    (What this is)
+        """Assigns points and keeps score.
 
         Args: 
             self (Player): An instance of Player.
@@ -92,7 +105,8 @@ class Dealer():
                 self.points = self.points - 75
 
     def card_update(self):
-        """    (What this is)
+        """Resets (shuffles) the card after each round. This 
+        makes the old card to guess high or low against.
 
         Args: 
             self (Player): An instance of Player.
